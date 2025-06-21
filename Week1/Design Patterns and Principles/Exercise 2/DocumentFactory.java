fb20cafeea5bbe54f1d87b2d6b48822f33245213
KanhaiyaKumarShukla <kanhaiyashukla0.01@gmail.com>
2025-06-21 23:36:28 +0530

    Re-added Exercise 2 as a normal directory (removed submodule)

---
+++ Week1/Design Patterns and Principles/Exercise 2/DocumentFactory.java
@@ -0,0 +1,11 @@
+public abstract class DocumentFactory {
+    public abstract Document createDocument();
+    
+    public void processDocument() {
+        Document doc = createDocument();
+        doc.open();
+        doc.save();
+        doc.close();
+        System.out.println("Processed: " + doc + "\n");
+    }
+}
